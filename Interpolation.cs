0eff8683d08ddfc794193f1e2f060f3d549c77a5
Normal_Pete <petrohorokh@gmail.com>
2024-04-03 17:52:13 +0300

    Add project files.

---
+++ Interpolation.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab5;
+
+public static class Interpolation
+{
+    public static List<List<double>> Difference(List<double> x, List<double> f)
+    {
+        var diff = new List<List<double>> { x, f };
+
+        for (int i = 2; i < x.Count + 1; i++)
+        {
+            var d = new List<double>();
+
+            for (int j = 0; j < diff[i - 1].Count - 1; j++)
+            {
+                Console.WriteLine(x[j + 1] - x[j]);
+                d.Add((diff[i - 1][j + 1] - diff[i - 1][j]) / (x[j + 1] - x[j]));
+            }
+
+            diff.Add(d);
+        }
+
+        return diff;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Lab5;
+
+var x = new List<double>();
+
+var h = .1;
+
+for (int i = 0; i < 10; i++) x.Add(i * h);
+
+var f = new List<double>{ .00000, .09983, .19866, .29552, .38941, .47942, .56464, .64421, .71735, .78332 };
+
+var p = new List<double>{ .226, .431, .669 };
+
+var diff = Interpolation.Difference(x, f);
+
+Console.WriteLine("\n");
